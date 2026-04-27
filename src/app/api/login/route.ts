4c3939601cdb5219fb93c450552d6122d01fd600
Toufique18 <toufiqueeamam7@gmail.com>
2026-04-27 16:19:33 +0600

    complete jwt authentication

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,17 +1,26 @@
 import { NextResponse } from 'next/server'
+import jwt from 'jsonwebtoken'
+
+const SECRET = 'mysecretkey' // later move to .env
 
 export async function POST(req: Request) {
   const { email, password } = await req.json()
 
   if (email === 'test@test.com' && password === '1234') {
+    const token = jwt.sign(
+      { email }, // payload
+      SECRET,
+      { expiresIn: '1h' }
+    )
+
     const res = NextResponse.json({ success: true })
 
-    res.cookies.set('token', 'abc123', {
+    res.cookies.set('token', token, {
       httpOnly: true,
     })
 
     return res
   }
 
-  return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
+  return NextResponse.json({ error: 'Invalid' }, { status: 401 })
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,47 @@
-'use client'
+// 'use client'
 
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
 
-export default function Dashboard() {
-  const router = useRouter()
+// export default function Dashboard() {
+//   const router = useRouter()
 
-  const handleLogout = async () => {
-    await fetch('/api/logout', { method: 'POST' })
-    router.push('/login')
+//   const handleLogout = async () => {
+//     await fetch('/api/logout', { method: 'POST' })
+//     router.push('/login')
+//   }
+
+//   return (
+//     <div>
+//       <h1>Dashboard</h1>
+//       <button onClick={handleLogout}>Logout</button>
+//     </div>
+//   )
+// }
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import * as jwt from 'jsonwebtoken'
+import LogoutButton from '../components/LogoutButton'
+const SECRET = 'mysecretkey'
+
+export default async function Dashboard() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get('token')?.value
+
+  if (!token) {
+    redirect('/login')
   }
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
-  )
+  try {
+    const user = jwt.verify(token, SECRET)
+
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome {(user as any).email}</p>
+        <LogoutButton />
+      </div>
+    )
+  } catch (err) {
+    redirect('/login')
+  }
 }
