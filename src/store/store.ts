68c5fcb2a7b494170d53dd827965a3e945f23470
Toufique18 <toufiqueeamam@gmail.com>
2026-04-28 15:01:23 +0600

    problem in infoslice

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
-// store/store.ts
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 import { apiSlice } from './apiSlice'
+import infoReducer from './infoSlice' // 👈 correct import
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    info: infoReducer, // ✅ correct
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
+
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
 })
